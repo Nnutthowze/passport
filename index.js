e6b7a3eb596414d40d2c395b77ee5369dbd3ebef
ronstarcool <reestronald@hotmail.com>
2017-03-25 13:17:55 +0100

    working on text-post

---
+++ index.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var LocalStrategy = require('passport-local');
+var User = require('./server/user');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.json());
+mongoose.connect("mongodb://localhost/vote");
+
+app.use(require('express-session')({
+    secret: "once i was a wooden boy",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+var isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    // other wise, send object to change router to login. of no concern right now.
+};
+
+app.get('/text1', function(req, res){
+    res.send({ text: 'this is text from simple request text1'});
+});
+
+app.post('/register', function(req, res){
+    console.log('/register is hit');
+    console.log(req.body);
+    const { username, password } = req.body;
+    var newUser = new User({ username });
+    User.register(newUser, password, function(err, user){
+        if(err){console.log(err)}
+        passport.authenticate('local')(req, res, function(){
+            res.send({text: 'you are now registered, not logged in'});
+        });
+    });
+});
+
+app.post("/login", function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.send({ text: "no user!"}); }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.send({ text: "you are logged in"});
+        });
+    })(req, res, next);
+});
+
+app.post('/text/new', isLoggedIn, function(req, res){
+    var text = req.body.text;
+
+});
+
+
+
+app.listen(4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            get: 'initial this.state',
+            user: {
+                username: '',
+                password: ''
+            },
+            text: ''
+        };
+    }
+
+    showTextOnRequest = () => {
+        let that = this;
+        axios.get('/text1').then(function(res){
+            that.setState({ get: res.data.text });
+        });
+    };
+    handleInputChange = (e) => {
+        const name = e.target.name;
+        const user = this.state.user;
+        user[name] = e.target.value;
+        this.setState({ user });
+        console.log(this.state.user);
+    };
+    signUp = () => {
+        axios({
+            method: 'post',
+            url: '/register',
+            data: this.state.user
+        }).then(function(res){
+            console.log(res);
+        });
+    };
+    login = () => {
+        axios({
+            method: 'post',
+            url: '/login',
+            data: this.state.user
+        }).then(function(res){
+            console.log(res);
+        });
+    };
+    handleInputChangeText = (e) => {
+        this.setState({ text: e.target.value });
+    };
+    postTextButton2 = () => {
+        axios({
+            method: 'post',
+            url: '/text/new',
+            data: this.state.text
+        }).then(function(res){
+            console.log(res);
+        });
+    };
+    showTextFromDb = () => {
+        this.setState({ get: 'hello, this is text from button 3'});
+    };
+
+    render() {
+        return (
+          <div className="app">
+              <div>
+                  <p>{this.state.get}</p>
+              </div>
+              <div>
+                  <button onClick={this.signUp}>sign up</button>
+                  <button onClick={this.login}>login</button>
+                  <p>username<input onChange={this.handleInputChange} type="text" name="username" /></p>
+                  <p>password<input onChange={this.handleInputChange} type="text" name="password"/></p>
+              </div>
+
+              <div>
+                  <button onClick={this.postTextButton2}>button 2: post to db</button>
+                  <p>put some text to save in db</p><input onChange={this.handleInputChangeText} type="text"/>
+              </div>
+              <div>
+                  <button onClick={this.showTextOnRequest}>button 1: show text on simple get request</button>
+                  <button onClick={this.showTextFromDb}>button 3: show text from db</button>
+              </div>
+          </div>
+        );
+    }
+}
+
+export default App;
