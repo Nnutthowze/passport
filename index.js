2f9b24ac2bfbfcfe15e98e9c50fdc5f726b02485
Nnutthowze <briareibismuth@gmail.com>
2017-03-25 16:13:18 +0300

    more es6 and minor fix

--- index.js
+++ index.js
@@ -1,87 +1,70 @@
-var express = require('express');
-var app = express();
-var passport = require('passport');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var LocalStrategy = require('passport-local');
-var User = require('./server/user');
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local');
+const User = require('./server/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.json());
-mongoose.connect("mongodb://localhost/vote");
+mongoose.connect("mongodb://localhost:27017/vote");
 
 app.use(require('express-session')({
     secret: "once i was a wooden boy",
     resave: false,
     saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
+app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     next();
 });
 
-var isLoggedIn = function(req, res, next){
-    if(req.isAuthenticated()){
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
         return next();
     }
     // other wise, send object to change router to login. of no concern right now.
 };
 
-app.get('/text1', function(req, res){
+app.get('/text1', (req, res) => {
     res.send({ text: 'this is text from simple request text1'});
 });
 
-app.post('/register', function(req, res){
-    console.log('/register is hit');
-    console.log(req.body);
+app.post('/register', (req, res) => {
     const { username, password } = req.body;
-    var newUser = new User({ username });
-    User.register(newUser, password, function(err, user){
-        if(err){console.log(err)}
-        passport.authenticate('local')(req, res, function(){
+    const newUser = new User({ username });
+    User.register(newUser, password, (err, user) => {
+        if(err) throw err;
+        passport.authenticate('local')(req, res, () => {
             res.send({text: 'you are now registered, not logged in'});
         });
     });
 });
 
-app.post("/login", function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
         if (err) { return next(err); }
         if (!user) { return res.send({ text: "no user!"}); }
-        req.logIn(user, function(err) {
+        req.logIn(user, (err) => {
             if (err) { return next(err); }
             return res.send({ text: "you are logged in"});
         });
     })(req, res, next);
 });
 
-app.post('/text/new', isLoggedIn, function(req, res){
-    var text = req.body.text;
-
+app.post('/text/new', isLoggedIn, (req, res) => {
+    const text = req.body.text;
 });
 
-
-
 app.listen(4000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- server/text.js
+++ server/text.js
@@ -8,4 +8,4 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports =  mongoose.mo
+module.exports =  mongoose.model('Text', TextSchema);
